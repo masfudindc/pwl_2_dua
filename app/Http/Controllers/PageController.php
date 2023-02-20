b67b0670a2fc72d7f7eacb23e866b280d205c00b
Masfudin Dwi Cahyo <masfudindc@gmail.com>
2023-02-20 10:20:49 +0700

    02_praktikum_web_lanjut_dua

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    // public function index(){
+    //     return 'Selamat Datang';
+    // }
+
+    // public function about(){
+    //     return 'Nama : Masfudin Dwi Cahyo, NIM : 2141720010';
+    // }
+
+    // public function articles($id){
+    //     return 'Halaman Artikel dengan Id '.$id;
+    // }
+}
